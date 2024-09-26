8e05e061206d7a06c8f03b956b5dae5d688055c2
NatoGA8 <nathangallagher16@gmail.com>
2024-09-26 18:18:17 +0100

    Add project files.

---
+++ Week1practical/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+BuildWeek1practical();
+
+void BuildWeek1practical ()
+{
+    Console.WriteLine("Please enter your name: ");
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Please enter your age? ");
+        int age =
+    int.Parse(Console.ReadLine());
+
+    int futureAge = age + 5;
+
+    Console.WriteLine($"Hello {name}, you will be {futureAge} in 5 years.");
+
+        }
